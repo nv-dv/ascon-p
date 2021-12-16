c61ac98a2758b92222227b444d3c6f500a606aa0
dorsal <dorsal1464@gmail.com>
2021-12-16 16:31:09 +0200

    changed d to masking_order

--- d_mask/dmask_AND_gate.h
+++ d_mask/dmask_AND_gate.h
@@ -2,62 +2,62 @@
 #include "../RandomBuffer/RandomBuffer.h"
 
 
-#if d==2
+#if MASKING_ORDER==2
 //insert d=2
 inline void refresh(dshare* p1, dshare* p2, RandomBuffer* rb) {
 	uint64_t r = rb->GetQWORD();
-	for (size_t i = 0; i < d; i++)
+	for (size_t i = 0; i < MASKING_ORDER; i++)
 	{
 		p1->s[i] ^= r;
 	}
 }
 #endif // d==2
-#if d==3
+#if MASKING_ORDER==3
 //insert d=3
 inline void refresh(dshare* p1, dshare* p2, RandomBuffer* rb) {
-	uint64_t r[d];
+	uint64_t r[MASKING_ORDER];
 	r[0] = rb->GetQWORD();
 	r[1] = rb->GetQWORD();
 	r[2] = r[0] ^ r[1];
-	for (size_t i = 0; i < d; i++)
+	for (size_t i = 0; i < MASKING_ORDER; i++)
 	{
 		p1->s[i] ^= r[i];
 	}
 }
 #endif // d==3
-#if d==4 || d==5
+#if MASKING_ORDER==4 || MASKING_ORDER==5
 	//insert d=4 or d=5
 inline void refresh(dshare* p1, dshare* p2, RandomBuffer* rb) {
-	uint64_t s[d];
+	uint64_t s[MASKING_ORDER];
 	uint64_t tmp;
 	// get random
-	for (size_t i = 0; i < d; i++) {s[i] = rb->GetQWORD();}
+	for (size_t i = 0; i < MASKING_ORDER; i++) {s[i] = rb->GetQWORD();}
 	tmp = s[d-1];
 	// rotate
-	for (size_t i = d - 1; i > 0; i--) {s[i] = s[i] ^ s[i - 1];}
+	for (size_t i = MASKING_ORDER - 1; i > 0; i--) {s[i] = s[i] ^ s[i - 1];}
 	s[0] = s[0] ^ tmp;
 	// apply random
-	for (size_t i = 0; i < d; i++) {p1->s[i] ^= s[i];}
+	for (size_t i = 0; i < MASKING_ORDER; i++) {p1->s[i] ^= s[i];}
 }
 #endif // d==4
 
-#if d==6
+#if MASKING_ORDER==6
 	//insert d=6
 inline void refresh(dshare* p1, dshare* p2, RandomBuffer* rb) {
-	uint64_t s[d];
+	uint64_t s[MASKING_ORDER];
 	uint64_t tmp;
 	// get random
-	for (size_t i = 0; i < d; i++) { s[i] = rb->GetQWORD(); }
-	tmp = s[d - 1];
+	for (size_t i = 0; i < MASKING_ORDER; i++) { s[i] = rb->GetQWORD(); }
+	tmp = s[MASKING_ORDER - 1];
 	// rotate
-	for (size_t i = d - 1; i > 0; i--) { s[i] = s[i] ^ s[i - 1]; }
+	for (size_t i = MASKING_ORDER - 1; i > 0; i--) { s[i] = s[i] ^ s[i - 1]; }
 	s[0] = s[0] ^ tmp;
 	// add half random
 	tmp = rb->GetQWORD();
 	s[0] ^= tmp;
 	s[3] ^= tmp;
 	// apply random
-	for (size_t i = 0; i < d; i++) { p1->s[i] ^= s[i]; }
+	for (size_t i = 0; i < MASKING_ORDER; i++) { p1->s[i] ^= s[i]; }
 }
 #endif
 
