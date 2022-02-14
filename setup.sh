164d14a849d09ee374126128416d9fd01da40f3b
Dor Salomon <56582984+dorsal1464@users.noreply.github.com>
2022-02-14 13:18:14 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,3 @@
 # python setup
 sudo apt-get install python3
-pip install scipy
-pip install matplotlib
-# enviorment setup
-sudo apt-get install g++ qemu-user-static qemu
-# for ARMv7
-sudo apt-get install g++-arm-linux-gnueabihf libc6-dev-armhf-cross
-# for AARCH64
-sudo apt-get install g++-aarch64-linux-gnu libc6-dev-arm64-cross
+sudo apt-get install g++
